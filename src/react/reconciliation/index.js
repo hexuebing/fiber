12b832af15afb889177d5b37b42c7249b14fb4e9
gift <test@example.com>
2022-11-12 19:23:32 +0800

    构建根节点的fiber对象

--- src/react/reconciliation/index.js
+++ src/react/reconciliation/index.js
@@ -1,16 +1,31 @@
 import { TaskQueue } from '../Misc'
 
 const taskQueue = new TaskQueue()
-const subTask = null
+let subTask = null
 
-const getFirstTask = () => {}
+const getFirstTask = () => {
+  const task = taskQueue.pop()
+  /**
+   * 返回最外层节点的fiber对象
+   */
+  return {
+    props: task.props,
+    stateNode: task.dom, // 节点的dom对象 或 组件实例对象
+    tag: "host_root", // 节点标记，对类型的分类
+    effects: [], // 存储需要更改的fiber对象
+    child: null, // 当前fiber对象的 子级 fiber
+  }
+}
 
-const executeTask = (fiber) => {}
+const executeTask = (fiber) => {
+  
+}
 
 const performTask = (deadline) => {
   if(!subTask) {
     // 任务为空去获取任务列表的第一个任务
     subTask = getFirstTask()
+    console.log(subTask)
   }
   while (subTask && deadline.timeRemaining() > 1){
     // 执行任务
